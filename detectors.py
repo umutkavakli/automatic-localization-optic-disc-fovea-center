91d6a96bccd84879bf6918c5cc836200ba0d0aeb
umut kavakli <umutkavakli.ukk@gmail.com>
2024-05-04 00:49:29 +0300

    OpticDiscDetector updated.
    - Ground Truth and kernel parameter added to constructor.
    - Predict function updated, saving image removed.
    - centerX, centerY --> center_x, center_y for naming convensions.
    - Returning x, y and image applied segmentation of fovea disc.

--- detectors.py
+++ detectors.py
@@ -5,57 +5,56 @@ import pandas as pd
 from heapq import heappush
 
 class OpticDiscDetector:
-    def __init__(self, image_path):
+    def __init__(self, image_path, ground_truth_path, kernel):
         self.paths = sorted(list(map(lambda x: os.path.join(image_path, x), os.listdir(image_path))))
+        self.ground_truths = pd.read_csv(ground_truth_path).iloc[:103, 1:3]
+        self.kernel = kernel
 
-    def predict(self, kernel, threshold_start=250, contour_limit=2, output='out'):
+    def predict(self, index, threshold_start=250, contour_limit=2):
         
-        for i, path in enumerate(self.paths):
-            # read image
-            image = cv2.imread(path)
+        # read image
+        image = cv2.imread(self.paths[index])
+            
+        # convert image into gray and apply historgram equalization
+        # since optic disc brighter, its value will be more close to channel limit (255)
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        equalized_image = cv2.equalizeHist(gray_image)
 
-            # convert image into gray and apply historgram equalization
-            # since optic disc brighter, its value will be more close to channel limit (255)
-            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            equalized_image = cv2.equalizeHist(gray_image)
+        # start min threhsold and contour list for iteration
+        contour_list = []
+        min_threshold = threshold_start
 
-            # start min threhsold and contour list for iteration
-            contour_list = []
-            min_threshold = threshold_start
+        # continue lowering min threshold and try to find contours
+        # until the number of contours reaches the limit
+        while len(contour_list) < contour_limit:
+            # apply thresholding into equalized image 
+            _, thresholded_image = cv2.threshold(equalized_image, min_threshold, 255, cv2.THRESH_BINARY)
+            
+            # apply erosion to remove structural degradation
+            erosion = cv2.erode(thresholded_image, self.kernel, iterations=2)
 
-            # continue lowering min threshold and try to find contours
-            # until the number of contours reaches the limit
-            while len(contour_list) < contour_limit:
-                # apply thresholding into equalized image 
-                _, thresholded_image = cv2.threshold(equalized_image, min_threshold, 255, cv2.THRESH_BINARY)
-                
-                # apply erosion to remove structural degradation
-                erosion = cv2.erode(thresholded_image, kernel, iterations=2)
+            # get contours
+            contours = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = contours[0] if len(contours) == 2 else contours[1]
 
-                # get contours
-                contours = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                contours = contours[0] if len(contours) == 2 else contours[1]
+            # calculate contour area to eliminate small ones
+            for c in contours:
+                area = cv2.contourArea(c)
+                if area > 5000:
+                    heappush(contour_list, (-area, c))
 
-                # calculate contour area to eliminate small ones
-                for c in contours:
-                    area = cv2.contourArea(c)
-                    if area > 5000:
-                        heappush(contour_list, (-area, c))
+            # lowering min threshold for next iteration
+            min_threshold -= 1
 
-                # lowering min threshold for next iteration
-                min_threshold -= 1
+        # apply segmentation to show optic disc
+        image[erosion == 255] = [255, 255, 255]
 
-            # apply segmentation to show optic disc
-            image[erosion == 255] = [255, 255, 255]
+        # find center of fovea using contour and draw a circle
+        center = cv2.moments(contour_list[0][1])
+        center_x = int(center["m10"] / center["m00"])
+        center_y = int(center["m01"] / center["m00"])
 
-            # find center of fovea using contour and draw a circle
-            center = cv2.moments(contour_list[0][1])
-            centerX = int(center["m10"] / center["m00"])
-            centerY = int(center["m01"] / center["m00"])
-            cv2.circle(image, (centerX, centerY), 9, (0, 0, 255), -1)
-
-            # save image
-            cv2.imwrite(f'{output}{i+1}.jpg', image)
+        return center_x, center_y, image
 
 class FoveaDetector:
     def __init__(self, image_path, ground_truth_path, kernel):
@@ -63,7 +62,7 @@ class FoveaDetector:
         self.ground_truths = pd.read_csv(ground_truth_path).iloc[:103, 1:3]
         self.kernel = kernel
 
-    def predict(self, index, threshold_start=220, contour_limit=2, roi_radius=700, output='out'):
+    def predict(self, index, threshold_start=220, contour_limit=2, roi_radius=700):
         
         # read image    
         image = cv2.imread(self.paths[index])
